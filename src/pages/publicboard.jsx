ff647f9d42b638b21818278961a0f010e2241b06
Ajoy10 <achu.ajoy10@gmail.com>
2023-04-12 17:07:27 +0530

    Unhid Quize from publicboard

--- src/pages/publicboard.jsx
+++ src/pages/publicboard.jsx
@@ -10,7 +10,7 @@ import ReactDOMServer from 'react-dom/server';
 import "./index.css"
 
 const remove_college_list = [{name:"Cultural Coordination Committee", location:"Manipal"}, {name:"Kasturba Hospital", location:"Manipal"}, {name:"MAHE", location:"Manipal"}];
-const remove_event_list = ["Staff Cooking: Vegetarian", "Staff Cooking: Non-Vegetarian", "Staff Cooking: Dessert", "Staff Vegetable & Fruit Carving", "Staff Variety Entertainment", "Poetry (Kannada)","Quiz"];
+const remove_event_list = ["Staff Cooking: Vegetarian", "Staff Cooking: Non-Vegetarian", "Staff Cooking: Dessert", "Staff Vegetable & Fruit Carving", "Staff Variety Entertainment", "Poetry (Kannada)"];
 // const remove_event_list = ["Some event", "Hello World"]; // Testing
 export default class extends React.Component {
   state = {
