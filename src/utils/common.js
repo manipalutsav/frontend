d9a3b14ca2c543a134bfbbc6214bc0ff46e48ac9
Varun Prahlad Balani <varunprahladb@gmail.com>
2023-04-12 09:18:01 +0530

    change edit timer

--- src/utils/common.js
+++ src/utils/common.js
@@ -92,7 +92,7 @@ export const isTeamChangeFreezed = () => {
     const curDate = new Date();
     // New time period for editing
     const editWindowBeginDate = new Date("11/April/2023, 10:00 PM");
-    const editWindowEndDate = new Date("12/April/2023, 9:00 AM");
+    const editWindowEndDate = new Date("12/April/2023, 12:00 PM");
     if(curDate > editWindowBeginDate && curDate <= editWindowEndDate){
         return false;
     }
