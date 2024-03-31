f2b345c55cc61f570f6ccd66505f64b7b6969a74
Ajoy10 <achu.ajoy10@gmail.com>
2024-03-31 12:38:23 +0530

    Changed back to normal countdown

--- src/components/Index/index.jsx
+++ src/components/Index/index.jsx
@@ -32,31 +32,44 @@ const event_renderer = ({ days, hours, minutes, seconds, completed }) => {
             The event is on! <span className="font-['Noto_Color_Emoji']">🎉</span>
         </div>
     </div>
-  }else{
-    return (
-      <div className="lg:m-4 sm:m-1">
-        <div className="flex gap-5 justify-center lg:scale-75  transition-all my-3">
-          <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
-            <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{days}</div>
-            <div className=" opacity-75 max-sm:text-xs">Day{days > 1?"s":""}</div>
-          </div>
-          <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
-            <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{hours}</div>
-            <div className=" opacity-75 max-sm:text-xs">Hour{hours > 1?"s":""}</div>
-          </div>
-          <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
-            <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{minutes}</div>
-            <div className=" opacity-75 max-sm:text-xs">Minute{minutes > 1?"s":""}</div>
-          </div>
-          <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
-            <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{seconds}</div>
-            <div className=" opacity-75 max-sm:text-xs">Second{seconds > 1?"s":""}</div>
-          </div>
-        </div>
-        <div className=" text-orange-700 lg:w-[40%] rounded-b m-auto">Until the excitement unfolds!</div>
-      </div>
-    )
   }
+  else{ // Text Countdown
+    if (days === 0 && hours === 0 && minutes === 0) {
+      return <span>Staff events start in: {seconds} seconds</span>;
+    } else if (days === 0 && hours === 0) {
+      return <span>Staff events start in: {minutes} minutes and {seconds} seconds</span>;
+    } else if (days === 0) {
+      return <span>Staff events start in: {hours} hours, {minutes} minutes and {seconds} seconds</span>;
+    } else {
+      return <span>Staff events start in: {days} days, {hours} hours, {minutes} minutes and {seconds} seconds</span>;
+    }
+  }
+  // Block countdown
+  // else{
+  //   return (
+  //     <div className="lg:m-4 sm:m-1">
+  //       <div className="flex gap-5 justify-center lg:scale-75  transition-all my-3">
+  //         <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
+  //           <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{days}</div>
+  //           <div className=" opacity-75 max-sm:text-xs">Day{days > 1?"s":""}</div>
+  //         </div>
+  //         <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
+  //           <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{hours}</div>
+  //           <div className=" opacity-75 max-sm:text-xs">Hour{hours > 1?"s":""}</div>
+  //         </div>
+  //         <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
+  //           <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{minutes}</div>
+  //           <div className=" opacity-75 max-sm:text-xs">Minute{minutes > 1?"s":""}</div>
+  //         </div>
+  //         <div className=" max-sm:gap-2 lg:w-[40%] max-w-[128px] lg:p-6 p-3 rounded-md bg-orange-200 gap-6 flex flex-col outline outline-orange-500">
+  //           <div id="ct-day" className=" text-3xl md:text-6xl font-['Barlow_Condensed'] scale-y-150">{seconds}</div>
+  //           <div className=" opacity-75 max-sm:text-xs">Second{seconds > 1?"s":""}</div>
+  //         </div>
+  //       </div>
+  //       <div className=" text-orange-700 lg:w-[40%] rounded-b m-auto">Until the excitement unfolds!</div>
+  //     </div>
+  //   )
+  // }
   
 };
 
@@ -88,7 +101,11 @@ export default () =>{
     <h2 className="mucapp">MAHE Utsav Coordinators App</h2>
     <h1 className="mucapp">{title}</h1>
     {/* <RegistrationTimer /> */}
-    <Countdown date={new Date("April 1, 2024, 10:30:00")} renderer={event_renderer}/>
+    <div className="mt-8">
+      <Countdown date={new Date("April 1, 2024, 10:30:00")} renderer={event_renderer}/>
+
+    </div>
+    {/* <div className=" text-orange-600 text-4xl m-2 my-4">Custom text here</div> */}
     {/* <div className="festival-live-message">The festival is on!</div> */}
     <img className="mucapp" css={{ width: "60%" }} alt="Logo" src={utsavLogo} />
   </div >
